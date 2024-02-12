b4583bc4032cf0d54a8dda469830547fe630f9c1
moinkhan07 <moin6k@gmail.com>
2024-02-12 14:52:29 +0530

    Project created

---
+++ XindusWishlistManagement/src/main/java/com/xinduswishlistmanagement/XindusWishlistManagementApplication.java
@@ -0,0 +1,13 @@
+package com.xinduswishlistmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XindusWishlistManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XindusWishlistManagementApplication.class, args);
+	}
+
+}
---
+++ XindusWishlistManagement/src/test/java/com/xinduswishlistmanagement/XindusWishlistManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinduswishlistmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XindusWishlistManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
